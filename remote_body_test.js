08549b94c510d7ad0595f49a443da8bfb1ed0c83
fengshun <fengshun.fan@funplus.com>
2022-05-01 16:01:45 +0800

    save

--- remote_body_test.js
+++ remote_body_test.js
@@ -1,11 +1,16 @@
-var body = $response.body;
-var obj = JSON.parse(body);
-console.log('================')
-console.log(obj)
-obj['result']['username'] = "12345678";
-obj['result']['count_like'] = 99999;
 
-modifiedBody = JSON.stringify(obj);
-setTimeout(function() { console.log("abc22222"); }, 1000);
-console.log(modifiedBody);
-$done(modifiedBody);
+function work() {
+    var body = $response.body;
+    var obj = JSON.parse(body);
+    console.log('================')
+    console.log(obj)
+    obj['result']['username'] = "12345678";
+    obj['result']['count_like'] = 99999;
+
+    modifiedBody = JSON.stringify(obj);
+    setTimeout(function () { console.log("abc22222"); }, 1000);
+    console.log(modifiedBody);
+    $done(modifiedBody);
+}
+
+work();
