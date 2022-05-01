c4c08c609ac0b22e4036cbf1eed8424791abab0d
fengshun <fengshun.fan@funplus.com>
2022-05-01 14:50:49 +0800

    save

--- remote_body_test.js
+++ remote_body_test.js
@@ -1,7 +1,7 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 
-obj['result'][username] = "12345678";
+obj['result']['username'] = "12345678";
 modifiedBody = JSON.stringify(obj);
 
 console.log(modifiedBody);
